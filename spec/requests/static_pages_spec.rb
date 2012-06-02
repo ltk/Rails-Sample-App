cf49e20348ed368f5bf7639aeb9c29645bcba2c9
Lawson Kurtz <lawson.kurtz@gmail.com>
2012-06-02 00:02:32 -0400

    Added a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,13 +43,13 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About Us")
+      page.should have_selector('title', text: "#{base_title} | About Us")
     end
   end
   describe "Contact page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+      page.should have_selector('h1', text: 'Contact')
     end
     
     it "should have the right title" do
