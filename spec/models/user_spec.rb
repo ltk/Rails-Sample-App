b175c690e3c44b2b2adc5d13af3f7cd5689530a2
Lawson Kurtz <lawson.kurtz@gmail.com>
2012-06-07 01:20:34 -0400

    refactored user model slightly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,5 +93,15 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+    
+    describe "email address with mixed case" do
+      let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+      
+      it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        @user.reload.email.should == mixed_case_email.downcase
+      end
+    end
   end
 end
